981e42180fa9a5f3354bb5181de45011db072ade
Andrii-Nikitich <nikitich141@gmail.com>
2024-12-25 21:30:23 +0200

    add index file

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-function makeTransaction(guantity, pricePerDroid) {
+function makeTransaction(guantity, pricePerDroid, customerCredits) {
+  // Загальна вартість замовлення
   const totalPrice = guantity * pricePerDroid;
-
-  return `You ordered ${guantity} droids worth ${totalPrice} credits!`;
+  // Перевірка можливості оплати
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${guantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
